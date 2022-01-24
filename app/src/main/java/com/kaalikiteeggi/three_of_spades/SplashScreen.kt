5acb7701b448d5584a809a9781a5602a172b2e26
Dushyant Kumbhat <kumbhat10@gmail.com>
2022-01-24 01:06:25 +0000

    Ver 69A 23/01/2022 -New splash screen & lighter - Vector game screen table added - Temporary commit - Gamescreen logic updated under single tree. - Resume from anywhere if crashed

--- app/src/main/java/com/kaalikiteeggi/three_of_spades/SplashScreen.kt
+++ app/src/main/java/com/kaalikiteeggi/three_of_spades/SplashScreen.kt
@@ -7,8 +7,6 @@ package com.kaalikiteeggi.three_of_spades
 //import com.google.ads.mediation.inmobi.InMobiConsent
 //import com.inmobi.sdk.InMobiSdk
 //import com.unity3d.ads.UnityAds
-import android.animation.Animator
-import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
@@ -20,11 +18,9 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
-import android.view.animation.AnimationUtils
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
@@ -36,206 +32,149 @@ import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.mopub.common.MoPub
 import com.mopub.common.SdkConfiguration
 import com.mopub.common.logging.MoPubLog
-import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.activity_game_screen.*
-import kotlinx.android.synthetic.main.activity_main_home_screen.*
 import kotlinx.android.synthetic.main.activity_splash_screen.*
 import java.util.concurrent.Executors
-import kotlin.random.Random
 
 class SplashScreen : AppCompatActivity() {
-	private var user: FirebaseUser? = null
-	private lateinit var appUpdateManager: AppUpdateManager
-	private lateinit var snackBar: Snackbar
-	private var handler = Handler(Looper.getMainLooper())
-	private var isAppLatest = false
-	private var isTimerOver = false
-	private var isNextActivityStarted = false
-	private var background = 4
-	private val timer = if (!BuildConfig.DEBUG) 4500L
-	else 4500L
-	private val timerLoading = timer - 1000L
-	private val requestCodeAppUpdate = 800
-	private lateinit var timeOverSound: MediaPlayer
-	private lateinit var backGroundSound: MediaPlayer
-	private lateinit var resultLauncher: ActivityResultLauncher<Intent>
-	override fun onCreate(savedInstanceState: Bundle?) {
-		setTheme(R.style.Theme_App)
-		super.onCreate(savedInstanceState)
-//		val splashScreen = installSplashScreen()
-//		splashScreen.setOnExitAnimationListener {
-//
-//		}
-		setContentView(R.layout.activity_splash_screen)
-//		splashRoot.background = ContextCompat.getDrawable(this, GameScreenData().tableBackground.random())
+    private var user: FirebaseUser? = null
+    private lateinit var appUpdateManager: AppUpdateManager
+    private lateinit var snackBar: Snackbar
+    private var handler = Handler(Looper.getMainLooper())
+    private var isAppLatest = false
+    private var isTimerOver = false
+    private var isNextActivityStarted = false
+    private var background = 4
+    private val timer = if (!BuildConfig.DEBUG) 4500L
+    else 4500L
+    private val requestCodeAppUpdate = 800
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_App)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
 
-//		splashBckgd.setImageResource(GameScreenData().tableBackground.random())
-//		background =  Random.nextInt(0, 6)
-//				when (background) {
-//		            0 -> splashBckgd.setImageResource(R.drawable.redblackburst)
-//		            4 -> splashBckgd.setImageResource(R.drawable.blueburst)
-//		            2 -> splashBckgd.setImageResource(R.drawable.greenyellowburst)
-//		            3 -> splashBckgd.setImageResource(R.drawable.navyblueburst)
-//		            1 -> splashBckgd.setImageResource(R.drawable.redorangeburst)
-//		            5 -> splashBckgd.setImageResource(R.drawable.yellowburst)
-//				}
-		val avd = (icon3.drawable as AnimatedVectorDrawable)
-		avd.registerAnimationCallback(object : Animatable2.AnimationCallback() {
-			override fun onAnimationStart(drawable: Drawable?) {
-				loadingSplash.visibility = View.VISIBLE
-				loadingSplash.startAnimation(AnimationUtils.loadAnimation(applicationContext, R.anim.blink_and_scale))
-//				icon3.startAnimation(AnimationUtils.loadAnimation(applicationContext, R.anim.zoomout_once))
-			}
-			override fun onAnimationEnd(drawable: Drawable?) {
-//				timeOverSound.start()
-				loadingSplash.postDelayed({
-					loadingSplash.text = getString(R.string.splashLoadingDone)
-				},400L)
-//				loadingProgress.isIndeterminate = false
-				loadingProgress.setProgressCompat(100, true)
-//				loadingProgress.progress = 100
-//				showUserProfile()
-			}
-		})
-		avd.start()
-		updateUI()
-		backGroundSound = MediaPlayer.create(applicationContext, R.raw.card_shuffle).apply { setVolume(0.13F, 0.13F) }
-		backGroundSound.start()
-		mobileAds()
-		resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
-			if (result.resultCode != Activity.RESULT_OK) {
-				checkAppUpdate()
-				toastCenter("App update failed \n Please try again or Restart app", duration = Snackbar.LENGTH_INDEFINITE)
-			} else if (result.resultCode == Activity.RESULT_OK) {
-				toastCenter("App was updated successfully", duration = Snackbar.LENGTH_SHORT) // dummy
-				isAppLatest = true
-				if (isTimerOver && !isNextActivityStarted) nextActivity()
-			}
-		}
+        val avd = (icon3.drawable as AnimatedVectorDrawable)
+        avd.registerAnimationCallback(object : Animatable2.AnimationCallback() {
+            override fun onAnimationStart(drawable: Drawable?) {
+                loadingSplash.visibility = View.VISIBLE
+            }
 
-		Executors.newSingleThreadExecutor().execute {
-			checkAppUpdate()
-		}
-		handler.postDelayed({
-			isTimerOver = true
-//			if (isAppLatest) nextActivity()
-		}, timer) // dummy 3500
-	}
+            override fun onAnimationEnd(drawable: Drawable?) {
+                loadingSplash.text = getString(R.string.splashLoadingDone)
+                loadingProgress.setProgressCompat(100, false)
+            }
+        })
+        avd.start()
+        loadUserProfile()
+        mobileAds()
+        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode != Activity.RESULT_OK) {
+                checkAppUpdate()
+                toastCenter("App update failed \n Please try again or Restart app", duration = Snackbar.LENGTH_INDEFINITE)
+            } else if (result.resultCode == Activity.RESULT_OK) {
+                toastCenter("App was updated successfully", duration = Snackbar.LENGTH_SHORT) // dummy
+                isAppLatest = true
+                if (isTimerOver && !isNextActivityStarted) nextActivity()
+            }
+        }
+        Executors.newSingleThreadExecutor().execute {
+            checkAppUpdate()
+        }
+        handler.postDelayed({
+            isTimerOver = true
+			if (isAppLatest) nextActivity()
+        }, timer) // dummy 3500
+    }
 
-	@SuppressLint("SetTextI18n")
-	private fun updateUI() {
-		timeOverSound = MediaPlayer.create(applicationContext, R.raw.timer_over).apply { setVolume(1F, 1F) }
-//		val loadingAnim = ObjectAnimator.ofInt(loading, "progress", 0, 10000)
-//		loadingAnim.duration = timerLoading //		loadingAnim.setInterpolator { object: BounceInterpolator() }
-//		loadingAnim.addListener(object : Animator.AnimatorListener {
-//			override fun onAnimationStart(animation: Animator?) {}
-//			override fun onAnimationEnd(animation: Animator?) {
-////				loadingSplash.text = "Lets Play"
-////				timeOverSound.start()
-//			}
-//			override fun onAnimationCancel(animation: Animator?) {}
-//			override fun onAnimationRepeat(animation: Animator?) {}
-//		})
-//		loadingAnim.start()
-		loadUserProfile()
-	}
-	private fun loadUserProfile(){
-		user = FirebaseAuth.getInstance().currentUser // dummy - could be a problem if invalid mAuth and not null
-		if (user != null) {
-			FirebaseCrashlytics.getInstance().setUserId(user!!.uid)
-//			userNameSplash.text = user?.displayName.toString().split(" ")[0]
-//			Picasso.get().load(user?.photoUrl.toString()).resize(300, 300).into(profilePic2)
-		}
-	}
+	private fun loadUserProfile() {
+        user = FirebaseAuth.getInstance().currentUser // dummy - could be a problem if invalid mAuth and not null
+        if (user != null) {
+            FirebaseCrashlytics.getInstance().setUserId(user!!.uid)
+        }
+    }
 
-	private fun mobileAds() {
-		//		MobileAds.initialize(this)
-		val configBuilder = if (BuildConfig.DEBUG) SdkConfiguration.Builder(getString(R.string.bannerTest_MP))
-			.withLogLevel(MoPubLog.LogLevel.DEBUG)
-		else SdkConfiguration.Builder(getString(R.string.bannerReal_MP))
-			.withLogLevel(MoPubLog.LogLevel.NONE)
+    private fun mobileAds() {
+        //		MobileAds.initialize(this)
+        val configBuilder = if (BuildConfig.DEBUG) SdkConfiguration.Builder(getString(R.string.bannerTest_MP)).withLogLevel(MoPubLog.LogLevel.DEBUG)
+        else SdkConfiguration.Builder(getString(R.string.bannerReal_MP)).withLogLevel(MoPubLog.LogLevel.NONE)
 
-		//		AdSettings.addTestDevice("bd40e50a-23b8-4798-8370-0ebbd6bf23fb") //onePlus
-		//		val facebookConfig = hashMapOf("banner" to "", "interstitial" to "")
-		//		AudienceNetworkAds.buildInitSettings(applicationContext).withPlacementIds(listOf("607386246545418_617997035484339")).withInitListener {
-		//			MoPub.initializeSdk(applicationContext, configBuilder.build()) { }
-		//		}.initialize()
-		MoPub.getPersonalInformationManager()?.grantConsent()
-		MoPub.initializeSdk(applicationContext, configBuilder.build()) {
+        //		AdSettings.addTestDevice("bd40e50a-23b8-4798-8370-0ebbd6bf23fb") //onePlus
+        //		val facebookConfig = hashMapOf("banner" to "", "interstitial" to "")
+        //		AudienceNetworkAds.buildInitSettings(applicationContext).withPlacementIds(listOf("607386246545418_617997035484339")).withInitListener {
+        //			MoPub.initializeSdk(applicationContext, configBuilder.build()) { }
+        //		}.initialize()
+        MoPub.getPersonalInformationManager()?.grantConsent()
+        MoPub.initializeSdk(applicationContext, configBuilder.build()) {
 //			val interstitialAdID = if (BuildConfig.DEBUG) getString(R.string.interstitialTest_mp) // real interstitial ad id - MoPub
 //			else getString(R.string.interstitialReal_mp) // test interstitial ad
 //			val mInterstitial = MoPubInterstitial(this, interstitialAdID)
 //			mInterstitial.load()
 //			val rewardedAdId = if (BuildConfig.DEBUG) getString(R.string.rewardedTest_mp) else getString(R.string.rewardedReal_mp)
 //			MoPubRewardedAds.loadRewardedAd(rewardedAdId)
-		}
-	}
+        }
+    }
 
-	private fun nextActivity() {
-		if (user != null && !isNextActivityStarted) {
-			isNextActivityStarted = true
-			handler.removeCallbacksAndMessages(null)
-			startActivity(Intent(this, MainHomeScreen::class.java).apply { putExtra("newUser", false) }
-				.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP))
-			overridePendingTransition(R.anim.slide_left_activity, R.anim.slide_left_activity)
-			finishAfterTransition()
-		} else if (!isNextActivityStarted) {
-			isNextActivityStarted = true
-//			LoginManager.getInstance().logOut()
-			startActivity(Intent(applicationContext, StartScreen::class.java).apply { putExtra("background", background) }
-				.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP))
-			overridePendingTransition(R.anim.slide_left_activity, R.anim.slide_left_activity)
-			finishAfterTransition()
-		}
-	}
+    private fun nextActivity() {
+        if (user != null && !isNextActivityStarted) {
+            isNextActivityStarted = true
+            handler.removeCallbacksAndMessages(null)
+            startActivity(Intent(this, MainHomeScreen::class.java).apply { putExtra("newUser", false) }.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP))
+            overridePendingTransition(R.anim.slide_left_activity, R.anim.slide_left_activity)
+            finishAfterTransition()
+        } else if (!isNextActivityStarted) {
+            isNextActivityStarted = true
+            startActivity(Intent(applicationContext, StartScreen::class.java).apply { putExtra("background", background) }.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP))
+            overridePendingTransition(R.anim.slide_left_activity, R.anim.slide_left_activity)
+            finishAfterTransition()
+        }
+    }
 
-	@Suppress("ReplaceJavaStaticMethodWithKotlinAnalog")
-
-	private fun checkAppUpdate() {
-		appUpdateManager = AppUpdateManagerFactory.create(baseContext)
-		val appUpdateInfoTask = appUpdateManager.appUpdateInfo
-		appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
-			if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
-				handler.removeCallbacksAndMessages(null)
-				toastCenter("New App version is available", duration = Snackbar.LENGTH_INDEFINITE)
-				appUpdateManager.startUpdateFlowForResult(appUpdateInfo, AppUpdateType.IMMEDIATE, this, requestCodeAppUpdate)
-			} else {
-				isAppLatest = true
+    @Suppress("ReplaceJavaStaticMethodWithKotlinAnalog")
+    private fun checkAppUpdate() {
+        appUpdateManager = AppUpdateManagerFactory.create(baseContext)
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+                handler.removeCallbacksAndMessages(null)
+                toastCenter("New App version is available\nPlease update the app", duration = Snackbar.LENGTH_INDEFINITE)
+                appUpdateManager.startUpdateFlowForResult(appUpdateInfo, AppUpdateType.IMMEDIATE, this, requestCodeAppUpdate)
+            } else {
+                isAppLatest = true
 				if (isTimerOver && !isNextActivityStarted) nextActivity()
-			}
-		}.addOnFailureListener {
-			//			toastCenter("App update check error \n${e.message.toString()}", duration = 1200)
-			isAppLatest = true
-			if (isTimerOver && !isNextActivityStarted) nextActivity()
-		}.addOnCompleteListener {
-			isAppLatest = true
-			if (isTimerOver && !isNextActivityStarted) nextActivity()
-		}
-	}
+            }
+        }.addOnFailureListener {
+            toastCenter("Failed to check App update \nPlease update from PlayStore", duration = 2000)
+            isAppLatest = true
+            if (isTimerOver && !isNextActivityStarted) nextActivity()
+        }.addOnCompleteListener {
+            isAppLatest = true
+            if (isTimerOver && !isNextActivityStarted) nextActivity()
+        }
+    }
 
-	private fun toastCenter(message: String, duration: Int = 1000) {
-		if (!this::snackBar.isInitialized) {
-			snackBar = Snackbar.make(findViewById(R.id.splashRoot), message, duration)
-				.setAction("Dismiss") { snackBar.dismiss() }
-			snackBar.setActionTextColor(getColor(R.color.borderblue))
-			snackBar.view.setOnClickListener { snackBar.dismiss() }
-		} else snackBar.setText(message)
-		snackBar.show()
-	}
+    private fun toastCenter(message: String, duration: Int = 1000) {
+        if (!this::snackBar.isInitialized) {
+            snackBar = Snackbar.make(findViewById(R.id.splashRoot), message, duration).setAction("Dismiss") { snackBar.dismiss() }
+            snackBar.setActionTextColor(getColor(R.color.borderblue))
+            snackBar.view.setOnClickListener { snackBar.dismiss() }
+        } else snackBar.setText(message)
+        snackBar.show()
+    }
 
-	override fun onResume() {
-		super.onResume()
-		appUpdateManager = AppUpdateManagerFactory.create(baseContext)
-		appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
-			if (appUpdateInfo.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
-				appUpdateManager.startUpdateFlowForResult(appUpdateInfo, AppUpdateType.IMMEDIATE, this, requestCodeAppUpdate)
-			}
-		}
-	}
-	override fun onDestroy() {
-		handler.removeCallbacksAndMessages(null)
-		MoPub.onDestroy(this)
-		super.onDestroy()
-	}
+    override fun onResume() {
+        super.onResume()
+        appUpdateManager = AppUpdateManagerFactory.create(baseContext)
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                appUpdateManager.startUpdateFlowForResult(appUpdateInfo, AppUpdateType.IMMEDIATE, this, requestCodeAppUpdate)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        handler.removeCallbacksAndMessages(null)
+        MoPub.onDestroy(this)
+        super.onDestroy()
+    }
 
 }
